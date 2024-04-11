44ba803972bc4cb4eb24edaa318a69288c1fd6ab
Veckote <veceliykot2@gmail.com>
2024-04-11 11:30:41 +0200

    oshibka

--- CursorBuy.java
+++ CursorBuy.java
@@ -1,19 +1,14 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class CursorBuy here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class CursorBuy extends Actor
 {
-    /**
-     * Act - do whatever the CursorBuy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public double cursorincome = 0;
     public void act()
     {
-        // Add your action code here.
+        if(Greenfoot.mouseClicked(this)){
+            if(Coockie.cash >= 10){
+                cursorincome = cursorincome + 0.1;
+            }
+        }
     }
 }
