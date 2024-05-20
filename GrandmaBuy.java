4b7f97ddfd975ebb08d9535956d443afae53ea51
Ostap <oss201612@gmail.com>
2024-05-20 08:02:16 +0200

    nigga

--- GrandmaBuy.java
+++ GrandmaBuy.java
@@ -1,24 +1,13 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.text.DecimalFormat;
-/**
- * Write a description of class GrandmaBuy here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class GrandmaBuy extends Actor
 {
-    /**
-     * Act - do whatever the GrandmaBuy wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    private int price = 100;
     DecimalFormat df = new DecimalFormat("#.#");
     public void act()
     {
         MyWorld w = new MyWorld();
         if(Greenfoot.mouseClicked(this)){
-            if(Coockie.cash >= w.cp){
+            if(Coockie.cash >= w.gp){
                 w.cps = w.cps + w.grandma;
                 Coockie.cash = Coockie.cash - w.gp;
                 w.gp = w.gp * 1.2;
