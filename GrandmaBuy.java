ba847d02ed09301e01580f9df8b7860907dc6850
Veckot <prominema@gmail.com>
2024-05-20 08:33:53 +0200

    bgs
    nigga

--- GrandmaBuy.java
+++ GrandmaBuy.java
@@ -12,13 +12,12 @@ public class GrandmaBuy extends Actor
      * Act - do whatever the GrandmaBuy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    private int price = 100;
     DecimalFormat df = new DecimalFormat("#.#");
     public void act()
     {
         MyWorld w = new MyWorld();
         if(Greenfoot.mouseClicked(this)){
-            if(Coockie.cash >= w.cp){
+            if(Coockie.cash >= w.gp){
                 w.cps = w.cps + w.grandma;
                 Coockie.cash = Coockie.cash - w.gp;
                 w.gp = w.gp * 1.2;
