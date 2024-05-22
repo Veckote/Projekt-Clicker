1ec88f621b578fdee6d10d868d07fcfdc5144c34
Veckot <prominema@gmail.com>
2024-05-22 16:11:44 +0200

    nigga

---
+++ Upgrade.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+import java.text.DecimalFormat;
+
+public class Upgrade extends World
+{
+    public Upgrade()
+    {    
+        super(1000, 600, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        BackBtn backBtn = new BackBtn();
+        addObject(backBtn,950,550);
+    }
+}
